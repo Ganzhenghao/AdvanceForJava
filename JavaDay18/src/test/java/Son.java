bd85ed6c9f355ba4eba6f551582a701af188cdc8
Ganzhenghao <2454023207@qq.com>
2021-03-17 20:24:17 +0800

    在Son类添加@EqualsAndHashCode注释

--- JavaDay18/src/test/java/Son.java
+++ JavaDay18/src/test/java/Son.java
@@ -5,7 +5,8 @@ import lombok.NoArgsConstructor;
 
 /**
  * @ClassName Son
- * @Description TODO
+ * @Description 如果为false 则子类equals()方法不会调用父类的equals方法 如果为true则会调用
+ *          else if (!super.equals(o)) {return false;}
  * @Author Ganzhenghao
  * @Date 2021/3/17 20:16
  * @Version 1.0
